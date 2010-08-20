f2a6bf28a48c7bd5150a40cf57e2f99460839e95
David Kahn <dk@structuralartistry.com>
2010-08-20 12:38:00 -0700

    first commit

---
+++ HTTPPostTestApp/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace HTTPPostTestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.txtPost = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtResponse = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(55, 12);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(619, 20);
+            this.txtURL.TabIndex = 0;
+            // 
+            // txtPost
+            // 
+            this.txtPost.Location = new System.Drawing.Point(55, 51);
+            this.txtPost.Multiline = true;
+            this.txtPost.Name = "txtPost";
+            this.txtPost.Size = new System.Drawing.Size(619, 273);
+            this.txtPost.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(599, 578);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "url";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "post";
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Location = new System.Drawing.Point(55, 345);
+            this.txtResponse.Multiline = true;
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.Size = new System.Drawing.Size(619, 227);
+            this.txtResponse.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-1, 348);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "response";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 613);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtResponse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPost);
+            this.Controls.Add(this.txtURL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.TextBox txtPost;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtResponse;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ HTTPPostTestApp/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;
+using System.Net;
+
+namespace HTTPPostTestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string RequestHttpPost(string URI, string PostData)
+        {
+            try
+            {
+                string strResponse = string.Empty;
+                WebRequest webRequest = null;
+                WebResponse webResponse = null;
+                StreamReader streamReader = null;
+
+                try
+                {
+                    webRequest = WebRequest.Create(URI);
+
+                    //req.Proxy = WebProxy.GetDefaultProxy(); // Enable if using proxy
+
+                    webRequest.Method = "POST";        // Post method
+
+                    webRequest.ContentType = "text/xml";     // content type
+
+                    // Wrap the request stream with a text-based writer
+                    StreamWriter streamWriter = new StreamWriter(webRequest.GetRequestStream());
+
+                    // Write the post data into the stream
+                    streamWriter.WriteLine(PostData);
+                    streamWriter.Close();
+                    // Send the data to the webserver
+                    webResponse = webRequest.GetResponse();
+
+                    //output the response to string
+                    streamReader = new StreamReader(webResponse.GetResponseStream());
+                    strResponse = streamReader.ReadToEnd();
+                    return strResponse;
+                }
+                catch (WebException WebAppErr)
+                {
+                    return WebAppErr.ToString();
+                }
+                catch (Exception AppErr)
+                {
+                    return AppErr.ToString();
+                }
+                finally
+                {
+                    if (webRequest != null) webRequest.GetRequestStream().Close();
+                    if (webResponse != null) webResponse.GetResponseStream().Close();
+                    if (streamReader != null) streamReader.Close();
+                }
+            }
+            catch (Exception AppErr)
+            {
+                return AppErr.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.txtResponse.Text = RequestHttpPost(this.txtURL.Text, this.txtPost.Text);
+        }
+    }
+}
